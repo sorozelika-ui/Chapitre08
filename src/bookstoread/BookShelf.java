2df13c1f19e591a16ea5c2d6bb0ff995d960d30a
sorozelika-ui <sorozelika@gmail.com>
2026-05-28 09:54:36 +0000

    methode add() implémentée

--- src/bookstoread/BookShelf.java
+++ src/bookstoread/BookShelf.java
@@ -1,9 +1,33 @@
-package bookstoread;
+/*package bookstoread;
 import java.util.Collections;
 import java.util.List;
 public class BookShelf {
     public List<String> books() {
         return Collections.emptyList();
+    }
+
+    public void add(String book) {
+
+    }
+}
+
+ */
+
+package bookstoread;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class BookShelf {
+    private final List<String> books = new ArrayList<>();
+
+    public List<String> books() {
+        return books;
+    }
+
+    public void add(String bookToAdd) {
+        books.add(bookToAdd);
     }
 }
 

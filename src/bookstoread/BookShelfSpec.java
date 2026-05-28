dd0b1870ccac1c823629bea88123ce5c2802a56f
sorozelika-ui <sorozelika@gmail.com>
2026-05-28 10:31:49 +0000

    ajout de la methode @BeforeEach

--- src/bookstoread/BookShelfSpec.java
+++ src/bookstoread/BookShelfSpec.java
@@ -1,4 +1,4 @@
-package bookstoread;
+/*package bookstoread;
 
 import java.util.List;
 
@@ -23,7 +23,7 @@ public class BookShelfSpec {
         assertEquals(2, books.size(), () -> "BookShelf should have two books.");
     }
 
-    */
+
 
     @Test
     void bookshelfContainsTwoBooksWhenTwoBooksAdded() {
@@ -44,6 +44,59 @@ public class BookShelfSpec {
     @Test
     void booksReturnedFromBookShelfIsImmutableForClient() {
         BookShelf shelf = new BookShelf();
+        shelf.add("Effective Java", "Code Complete");
+        List<String> books = shelf.books();
+        try {
+            books.add("The Mythical Man-Month");
+            fail(() -> "Should not be able to add book to books");
+        } catch (Exception e) {
+            assertTrue(e instanceof UnsupportedOperationException, () -> "Should throw UnsupportedOperationException.");
+        }
+    }
+}
+*/
+
+
+package bookstoread;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class BookShelfSpec {
+    private BookShelf shelf;
+
+    @BeforeEach
+    void init() throws Exception {
+        shelf = new BookShelf();
+    }
+
+    @Test
+    public void shelfEmptyWhenNoBookAdded() throws Exception {
+        List<String> books = shelf.books();
+        assertTrue(books.isEmpty(), () -> "BookShelf should be empty.");
+    }
+
+    @Test
+    void bookshelfContainsTwoBooksWhenTwoBooksAdded() {
+        shelf.add("Effective Java", "Code Complete");
+        List<String> books = shelf.books();
+        assertEquals(2, books.size(), () -> "BookShelf should have two books.");
+    }
+
+    @Test
+    public void emptyBookShelfWhenAddIsCalledWithoutBooks() {
+        shelf.add();
+        List<String> books = shelf.books();
+        assertTrue(books.isEmpty(), () -> "BookShelf should be empty.");
+    }
+
+    @Test
+    void booksReturnedFromBookShelfIsImmutableForClient() {
         shelf.add("Effective Java", "Code Complete");
         List<String> books = shelf.books();
         try {

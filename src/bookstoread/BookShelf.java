132b3ed1ecec7024f29d4010d1c9c97f266016b6
sorozelika-ui <sorozelika@gmail.com>
2026-06-08 10:46:27 +0000

    regroupement par criteres
    Signed-off-by: Nobou Claude <nobouclodio@gmail.com>

--- src/bookstoread/BookShelf.java
+++ src/bookstoread/BookShelf.java
@@ -2,6 +2,7 @@ package bookstoread;
 
 import java.time.Year;
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class BookShelf {
@@ -51,5 +52,9 @@ public class BookShelf {
     public Map<Year, List<Book>> groupByPublicationYear() {
         return  books.stream().collect(Collectors.groupingBy(book ->
                 Year.of(book.getPublishedOn().getYear())));
+    }
+
+    public <K> Map<K, List<Book>> groupBy(Function<Book, K> fx) {
+        return books.stream().collect(Collectors.groupingBy(fx));
     }
 }
